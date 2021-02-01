356f948b0352ca345c42ec76853467589356d1c1
Elbert Corniell <elberthcorniell@gmail.com>
2021-02-01 12:11:44 -0400

    Created app component

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import BooksList from './BooksList';
+import BooksForm from './BooksForm';
+
+const App = () => (
+  <div className="App">
+    <BooksList />
+    <BooksForm />
+  </div>
+);
+
+export default App;
