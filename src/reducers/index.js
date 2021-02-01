01158e23e59e7218079eee90ab42e0ca29fb49c0
Elbert Corniell <elberthcorniell@gmail.com>
2021-02-01 12:28:49 -0400

    Update index.js

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,6 @@
 import { combineReducers, createStore } from 'redux';
 import books from './books';
 
-// Use ES6 object literal shorthand syntax to define the object shape
 const rootReducer = combineReducers({
   books,
 });
